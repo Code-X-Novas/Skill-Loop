b1ca5224cf3152e721585aa93eca3f834c971240
Om Thanage <om.thanage@somaiya.edu>
2025-07-01 08:34:48 +0530

    fix:  Testimonials length

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 const testimonials = [
@@ -7,7 +7,7 @@ const testimonials = [
     name: "John Doe",
     role: "Software Engineer",
     image: "https://picsum.photos/id/1005/300",
-    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error nisi necessitatibus culpa soluta fugit ut rerum vitae nam, enim facilis praesentium totam eveniet ducimus magni voluptas dolor quam. Magni eius ratione, veniam odit, vel quos perspiciatis accusamus iste sunt in rerum corporis! Laudantium hic iure nesciunt consequatur optio reiciendis error.`,
+    text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.`,
   },
   {
     id: 2,
@@ -27,6 +27,17 @@ const testimonials = [
 
 const Testimonials = () => {
   const [activeIndex, setActiveIndex] = useState(1);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const next = () => {
     setActiveIndex((prev) => (prev + 1) % testimonials.length);
@@ -47,7 +58,7 @@ const Testimonials = () => {
           whileInView={{ opacity: 1, x: 0, y: 0 }}
           transition={{ duration: 0.5, ease: "easeOut" }}
           viewport={{ once: true, amount: 0.5 }}
-          className="md:text-6xl text-4xl font-bold text-gray-900 mb-8">Testimonials</motion.h1>
+          className="md:text-6xl text-4xl font-bold text-gray-900 md:mb-8 mb-4">Testimonials</motion.h1>
         <div className="justify-center relative flex flex-col items-center md:gap-8 gap-2">
             
             <motion.div 
@@ -77,13 +88,13 @@ const Testimonials = () => {
                 </div>
 
                 <div className="bg-white">
-                    <div className="bg-[#F9F6F0] p-6 rounded-tl-[150px] flex md:flex-row flex-col items-center justify-around md:px-24 px-4 py-12 gap-8">
+                    <div className="bg-[#F9F6F0] p-6 rounded-tl-[150px] flex md:flex-row flex-col items-center justify-around md:px-24 px-4 md:py-12 py-2 md:gap-8 gap-4">
                         <div className="relative">
-                            <div className="bg-[#E28050]/50 rounded-md p-1 -mb-3 z-0 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:w-[200px] md:h-[340px] sm:w-[100px] sm:h-[240px] w-[50px] h-[190px]"></div>
+                            <div className="bg-[#E28050]/50 rounded-md p-1 -mb-3 z-0 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:w-[200px] md:h-[340px] sm:w-[100px] sm:h-[240px] w-[50px] h-[140px]"></div>
                             <img
                                 src={active.image}
                                 alt={active.name}
-                                className="rounded-lg relative z-10 md:w-[300px] md:h-[300px] sm:w-[200px] sm:h-[200px] w-[150px] h-[150px] object-cover "
+                                className="rounded-lg relative z-10 md:w-[300px] md:h-[300px] sm:w-[200px] sm:h-[200px] w-[100px] h-[100px] object-cover "
                             />
                         </div>
                         <div className="text-gray-600 text-sm mt-8 md:mt-0 text-left max-w-xl transition-all duration-500 ease-in-out">
@@ -94,7 +105,7 @@ const Testimonials = () => {
 
                 <div className="relative">
                     <span className="absolute bottom-0 left-0 transform -translate-x-1/2 translate-y-1/2 sm:h-48 sm:w-48 h-16 w-16 rounded-full bg-[#E28050] z-5 blur-sm opacity-50 "></span>
-                    <div className="relative flex justify-center items-center gap-6 py-6 overflow-clip">
+                    <div className="relative flex justify-center items-center gap-6 md:py-6 py-4 overflow-clip">
                     <span className="absolute bottom-0 left-0 transform -translate-x-1/2 translate-y-1/2 sm:h-48 sm:w-48 h-16 w-16 md:border-[1rem] border-4 rounded-full border-[#E28050] bg-[#F9F6F0] z-5"></span>
                     
                     <button
@@ -114,7 +125,7 @@ const Testimonials = () => {
                             
                             const isActive = position === 0;
                             const opacity = Math.abs(position) > 1 ? 0 : 1;
-                            const translateX = position * 80;
+                            const translateX = windowWidth > 480 ? position * 80 : position * 50;
                             
                             return (
                                 <img
@@ -122,7 +133,7 @@ const Testimonials = () => {
                                     src={t.image}
                                     alt={`Avatar ${t.name}`}
                                     className={`rounded-full cursor-pointer transition-all duration-500 ease-in-out absolute ${
-                                        isActive ? "sm:h-24 sm:w-24 h-18 w-18 opacity-100" : "sm:h-12 sm:w-12 h-9 w-9 opacity-40"
+                                        isActive ? "sm:h-24 sm:w-24 h-12 w-12 opacity-100" : "sm:h-12 sm:w-12 h-6 w-6 opacity-40"
                                     }`}
                                     style={{
                                         transform: `translateX(${translateX}px)`,
