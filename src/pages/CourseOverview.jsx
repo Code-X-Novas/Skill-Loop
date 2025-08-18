ba6d58813b8c21128ea2899c6637b92574e5331b
Nabin Agrawal <nabinagrawal64@gmail.com>
2025-08-18 08:27:03 +0530

    change key

--- src/pages/CourseOverview.jsx
+++ src/pages/CourseOverview.jsx
@@ -322,10 +322,8 @@ const CourseOverview = () => {
             const courseTitle = `${course.title} - ${subCourse.level}`;
             const coursePrice = subCourse.price;
 
-            //my : rzp_test_BvqFX2Hzptda8Z
-
             const orderOptions = {
-                key: "rzp_test_bEgUYtg6yXPfNV", 
+                key: "rzp_live_R6RdoFgHAMaOX7", 
                 amount: parseInt(coursePrice * 100),
                 currency: "INR",
                 name: "SkillLoop",
